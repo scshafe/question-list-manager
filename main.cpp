5d38ec8445574c654cc81e5e168f9a706331a7d9
scshafe <scshafe@umich.edu>
2024-12-07 01:37:53 -0800

    testing command line for help options

--- main.cpp
+++ main.cpp
@@ -12,6 +12,27 @@ int main(int argc, char **argv)
   po::options_description desc("Allowed options");
 
 
+  desc.add_options()
+      ("help", "produce help message")
+      // ("compression", po::value<int>(), "set compression level")
+  ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);    
+
+  if (vm.count("help")) {
+      std::cout << desc << "\n";
+      return 1;
+  }
+
+  // if (vm.count("compression")) {
+  //     cout << "Compression level was set to " 
+  // << vm["compression"].as<int>() << ".\n";
+  // } else {
+  //     cout << "Compression level was not set.\n";
+  // }
+
   if (__cplusplus == 202101L) std::cout << "C++23";
     else if (__cplusplus == 202002L) std::cout << "C++20";
     else if (__cplusplus == 201703L) std::cout << "C++17";
