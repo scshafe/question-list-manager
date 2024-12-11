c2f0d10e31259a7107a1527eb4906a5b255c007f
scshafe <scshafe@umich.edu>
2024-12-10 22:56:16 -0800

    added env variable support for database in startup script

--- start_ms_application.sh
+++ start_ms_application.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
-/home/debian/EMBEDDED-GUI --database-file /home/debian/motion-sensor-configuration/vocabulary-words-test-argument.json
+VOCAB_FILE=""
+
+if [[ -z "$MS_VOCAB_DATABASE" ]]; then
+  VOCAB_FILE="/home/debian/motion-sensor-configuration/vocabulary-words-test-argument.json"
+  echo "Using default vocabulary database file: $VOCAB_FILE"
+else
+  VOCAB_FILE="$MS_VOCAB_DATABASE"
+  echo "Using custom vocabulary database file path: $VOCAB_FILE"
+fi
+
+
+
+/home/debian/EMBEDDED-GUI --database-file $VOCAB_FILE
