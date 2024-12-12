b08130bb9206a3a91ef17ddf8c4ada37e4afec63
scshafe <scshafe@umich.edu>
2024-12-11 23:44:34 -0800

    added absolute/relative path handling for xinit startup script

--- start_ms_application.sh
+++ start_ms_application.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 
+
+
+# check to see if the user has set an environment variable to override default database location
 VOCAB_FILE=""
 
 if [[ -z "$MS_VOCAB_DATABASE" ]]; then
+  if [[ "$MS_VOCAB_DATABASE" != /* ]]; then
+    echo "Error: $MS_VOCAB_DATABASE is relative. Must use absolute path for database override"
+    return 1
+  fi
+  
   VOCAB_FILE="/home/debian/motion-sensor-configuration/vocabulary-words-test-argument.json"
   echo "Using default vocabulary database file: $VOCAB_FILE"
 else
@@ -10,6 +18,15 @@ else
   echo "Using custom vocabulary database file path: $VOCAB_FILE"
 fi
 
+EXECUABLE_FILE="/home/debian/EMBEDDED-GUI"
 
+if [[ -z "$MS_EXECUTABLE"]]; then
+  EXECUABLE_FILE="$MS_EXECUTABLE"
+fi
 
-/home/debian/EMBEDDED-GUI --database-file $VOCAB_FILE
+if [[ "$EXECUTABLE_FILE" != /* ]]; then
+  echo "Error: $EXECUTABLE_FILE is relative. Must use absolute path for executable path"
+  return 1
+fi
+
+$EXECUABLE_FILE --database-file $VOCAB_FILE
